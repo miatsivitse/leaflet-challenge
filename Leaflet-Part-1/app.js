25e54330d467db9b737985730ac196d96c8f4a9f
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 <galluccimia@gmail.com>
2023-01-07 12:47:32 -0500

    bas elayers

--- Leaflet-Part-1/app.js
+++ Leaflet-Part-1/app.js
@@ -1,7 +1,16 @@
-  // Create map
-  var myMap = L.map("map", {
+// Create map
+var myMap = L.map("map", {
     center: [
-    40.7608, 111.8910
+        40.7608, 111.8910
     ],
     zoom: 5,
-  });
+});
+
+// Create base layers
+var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+})
+
+var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+});
