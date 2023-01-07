8afff785218c1c08fdca7d66fc3f795ac3f38be2
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 <galluccimia@gmail.com>
2023-01-07 15:36:33 -0500

    add baseMaps and Overlay objects

--- Leaflet-Part-1/app.js
+++ Leaflet-Part-1/app.js
@@ -1,27 +1,24 @@
-// Store API endpoint as queryUrl.
+// Store API endpoint as queryUrl
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-// Perform a GET request to the query URL/
+// Perform a GET request 
 d3.json(queryUrl).then(function (data) {
-    // send the data.features object to the createFeatures function
     createFeatures(data.features);
   });
 
   function createFeatures(earthquakeData) {
 
-    // Define a function that we want to run once for each feature in the features array.
-    // Give each feature a popup that describes the place and time of the earthquake.
+    // Define function, give each feature a popup
     function onEachFeature(feature, layer) {
       layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
     }
   
-    // Create a GeoJSON layer that contains the features array on the earthquakeData object.
-    // Run the onEachFeature function once for each piece of data in the array.
+    // Create a GeoJSON layer, run the onEachFeature function
     var earthquakes = L.geoJSON(earthquakeData, {
       onEachFeature: onEachFeature
     });
   
-    // Send our earthquakes layer to the createMap function/
+    // Send earthquakes layer to the createMap function
     createMap(earthquakes);
   }
   
@@ -32,18 +29,24 @@ d3.json(queryUrl).then(function (data) {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     });
 
+  // Create a baseMaps object
+  var baseMaps = {
+    "Street Map": street,
+  };
+
+  // Create an overlay object 
+  var overlayMaps = {
+    Earthquakes: earthquakes
+  };
+
 // Create map
 var myMap = L.map("map", {
-    center: [
-        40.7608, 111.8910
-    ],
+    center: [40.7608, -111.8910],
     zoom: 5,
     layers: [street]
 });
 
-  // Create a layer control.
-  // Pass it our baseMaps and overlayMaps.
-  // Add the layer control to the map.
+  // Create a layer control and add to map
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
   }).addTo(myMap);
